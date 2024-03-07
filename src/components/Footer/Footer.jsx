96c5823d48809086f0f113646a95a63e3b46bc65
pardesisaksham <saksham.pardesi@zappian.com>
2024-03-07 19:34:39 +0530

    reduxToolkit implemented and bug fixes for authJs

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import {} from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import {} from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
