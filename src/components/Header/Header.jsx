6e61255c4e15e735cfb85651bc909ca629f42e00
pardesisaksham <saksham.pardesi@zappian.com>
2024-03-10 15:01:49 +0530

    login signup page complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,70 @@
-import {} from 'react'
+import { Fragment } from "react";
+import { LogoutButton, Container } from "../../components";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <Fragment>
+      <header className="py-3 shadow bg-gray-500">
+        <Container>
+          <nav>
+            <div className="mr-4">
+              <Link to="/">Logo</Link>
+            </div>
+            <ul className="flex ml-auto">
+              {navItems.map((item) =>
+                item.active ? (
+                  <li key={item.name} className="mr-4">
+                    <button
+                      onClick={() => navigate(item.slug)}
+                      className="inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full"
+                    >
+                      {item.name}
+                    </button>
+                  </li>
+                ) : (
+                  ""
+                )
+              )}
+              {authStatus && (<li>
+                <LogoutButton/>
+              </li>)}
+            </ul>
+          </nav>
+        </Container>
+      </header>
+    </Fragment>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-export { Header, Footer };
+import Container from "./container/Container";
+import LogoutButton from "./Header/LogoutButton";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import SignUp from "./SignUp";
+export { Header, Footer, Container, LogoutButton, Login, Button, Input,SignUp };
